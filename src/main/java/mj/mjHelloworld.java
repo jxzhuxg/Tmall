aa2000f6055fa02de4f99b33a803d08e0933b4bb
jxzhuxg <jxzhuxingguo@126.com>
2018-07-25 14:33:00 +0800

    23322

--- src/main/java/mj/mjHelloworld.java
+++ src/main/java/mj/mjHelloworld.java
@@ -1,5 +1,7 @@
 package mj;
 
 public class mjHelloworld {
-
+	public static void main(String[] args) {
+		System.out.println("adsfasdfasd");
+	}
 }
--- src/test/java/aa/Helloworld.java
+++ src/test/java/aa/Helloworld.java
@@ -1,8 +1,13 @@
 package aa;
 
 public class Helloworld {
+
+	public static void test1(int i){
+		System.out.println(i);
+	}
 	public static void main(String[] args) {
 		System.out.println();
-		
+		test1(2);
+
 	}
 }
