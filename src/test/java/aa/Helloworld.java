428942995dca38c6716911d773bcc3db890f3974
aa <aa@aa.com>
2018-07-25 14:52:43 +0800

    4444

--- src/test/java/aa/Helloworld.java
+++ src/test/java/aa/Helloworld.java
@@ -1,5 +1,7 @@
 package aa;
 
+import java.util.Random;
+
 public class Helloworld {
 
 	public static void test1(int i){
@@ -8,6 +10,12 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println();
 		test1(2);
+		test2(2);
 
+	}
+	public static void test2(int i){
+		double rr = 
+		new Random().nextDouble();
+		System.out.println(i*rr);
 	}
 }
